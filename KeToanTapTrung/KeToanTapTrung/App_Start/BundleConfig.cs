6aa60c93c2b58f06a278596aa6f0350732c3fcb1
Nguyen Viet Tan <tan1812003@gmail.com>
2019-03-22 10:41:52 +0700

    Them thu vien chung

--- KeToanTapTrung/KeToanTapTrung/App_Start/BundleConfig.cs
+++ KeToanTapTrung/KeToanTapTrung/App_Start/BundleConfig.cs
@@ -8,6 +8,26 @@ namespace KeToanTapTrung
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/css/common").Include(
+                       "~/Content/flatlab/css/bootstrap.min.css",
+                       "~/Content/flatlab/css/bootstrap-reset.css",
+                       "~/Content/flatlab/assets/font-awesome/css/font-awesome.css",
+                       "~/Content/flatlab/css/jquery.steps.css",
+                       "~/Content/flatlab/assets/bootstrap-datepicker/css/datepicker.css",
+                       "~/Content/flatlab/assets/select2/css/select2.min.css",
+                       "~/Content/flatlab/assets/toastr-master/toastr.css",
+                       "~/Content/flatlab/assets/summernote/dist/summernote.css",
+                       "~/Content/flatlab/css/tasks.css"
+                       ));
+
+
+            bundles.Add(new StyleBundle("~/css/plug-in").Include(
+                       "~/Content/politespace/css/politespace.css",
+                       "~/Content/datatables/1.10.18/css/dataTables.bootstrap.min.css"
+                       //"~/Content/datatables/rowgroup/1.0.3/css/rowGroup.bootstrap.min.css"
+                       //"~/Content/flatlab/assets/data-tables/DT_bootstrap.css"
+                       ));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
---
+++ KeToanTapTrung/KeToanTapTrung/Content/politespace/js/politespace-init.js
@@ -0,0 +1,30 @@
+/*! politespace - v1.0.2 - 2017-07-06
+Politely add spaces to input values to increase readability (credit card numbers, phone numbers, etc).
+ * https://github.com/filamentgroup/politespace
+ * Copyright (c) 2017 Filament Group (@filamentgroup)
+ * MIT License */
+
+(function( win ) {
+	"use strict";
+
+	var $;
+	if( 'shoestring' in win ) {
+		$ = win.shoestring;
+	} else if( 'jQuery' in win ) {
+		$ = win.jQuery;
+	} else {
+		throw new Error( "politespace: DOM library not found." );
+	}
+
+	// auto-init on enhance (which is called on domready)
+    $(document).bind( "enhance-politespace", function( e ) {
+        var $sel = $(e.target).is("[data-politespace]") ? $(e.target) : $("[data-politespace]", e.target);
+        $sel.politespace();
+    });
+
+    $(document).bind("refresh-politespace", function (e) {
+        var $sel = $(e.target).is("[data-politespace]") ? $(e.target) : $("[data-politespace]", e.target);
+        $sel.trigger("politespace-show-proxy");
+    });
+
+})( typeof window !== "undefined" ? window : this );
